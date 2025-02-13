9cbd733d1aeb550eaf38c4b73524f338fd873989
diegomunizdev <diegomunizdev@outlook.com>
2025-02-13 15:33:50 -0300

    main: commit

--- src/services/websocket/upload.service.ts
+++ src/services/websocket/upload.service.ts
@@ -7,10 +7,8 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import { v4 as uuidv4 } from 'uuid';
-import { Readable } from 'stream';
 
 @WebSocketGateway(3002, {
   cors: {
@@ -24,8 +22,9 @@ export class UploadGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   constructor() {
     this.s3 = new S3Client({
-      endpoint: 'http://localhost:4566',
+      endpoint: 'http://127.0.0.1:4566',
       region: 'sa-east-1',
+      forcePathStyle: true, // Importante para compatibilidade com LocalStack
       credentials: {
         accessKeyId: 'admin',
         secretAccessKey: 'admin',
@@ -34,39 +33,36 @@ export class UploadGateway implements OnGatewayConnection, OnGatewayDisconnect {
   }
   // Quando um cliente se conecta
   handleConnection(client: any) {
-    console.log('Cliente conectado', client.id);
+    console.log('Cliente conectado');
   }
 
   // Quando um cliente se desconecta
   handleDisconnect(client: any) {
-    console.log('Cliente desconectado', client.id);
+    console.log('Cliente desconectado');
   }
 
-  @SubscribeMessage('upload')
-  async handleUploadVideo(@MessageBody() videoBlob: any) {
+  @SubscribeMessage('message')
+  async handleUploadVideo(@MessageBody() data: Buffer) {
     try {
       // Converte o vídeo (blob) para um stream legível
-      const videoStream = Readable.from(videoBlob);
-
       // Configuração do S3 para upload do vídeo
       const params = {
-        Bucket: 'your-bucket-name',
-        Key: `videos/${Date.now()}.webm`, // Nome do arquivo no S3
-        Body: videoStream,
+        Bucket: 'mvp-record',
+        Key: `stream/${Date.now()}.webm`, // Nome do arquivo no S3
+        Body: data,
         ContentType: 'video/webm',
       };
 
       // Realiza o upload do vídeo para o S3
       const command = new PutObjectCommand(params);
-      const data = await this.s3.send(command);
+      const saved = await this.s3.send(command);
 
-      console.log('Upload bem-sucedido', data);
-
+      console.log('Upload bem-sucedido', saved);
       // Envia uma confirmação de sucesso para o cliente
       return { message: 'Upload bem-sucedido!' };
     } catch (error) {
       console.error('Erro no upload:', error);
-      return { message: 'Erro no upload' };
+      return { message: 'Erro no upload', error };
     }
   }
 }
